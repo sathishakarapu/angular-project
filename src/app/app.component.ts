e725374af8ea6687add4cdb6d73c6aa6b63cdc2f
sathishakarapu <akarapusathish003@gmail.com>
2025-02-19 12:14:24 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,81 +1,109 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, inject, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
 @Component({
   selector: 'app-root',
-  imports: [FormsModule],
+  imports: [CommonModule,FormsModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
+  
+  public isVisible = true;
+  public isEditing = false;
+  public editIndex: number | null = null;
+
+  ngOnInit() {
+    const storedUsers = localStorage.getItem('users');
+    if (storedUsers) {
+      this.users = JSON.parse(storedUsers);
+    }
+  }
+
+  changeView(){
+    this.isVisible = !this.isVisible;
+  }
+
   userObject: User = new User();
-  isEditing: boolean = false;
-  userList: User[] = [];
 
-  http = inject(HttpClient);
-
-  onSave() {
-    this.http.post("http://localhost:3000/userList",this.userObject).subscribe((res:any) => {
-      this.getData();
+  saveUser() {
+    if (!this.isEditing) {
+      const newUser = { ...this.userObject, id: (this.users.length + 1).toString() };
+      this.users.push(newUser);
+      this.updateLocalStorage();
       this.clearForm();
-    });
+      this.changeView();
+    }
   }
 
-  ngOnInit(): void {
-    this.getData();
-  }
-
-  getData(){
-    this.http.get("http://localhost:3000/userList").subscribe((res:any) => {
-      this.userList = res;
-    })
-  };
-
-  onEdit(id:number) {
-    this.isEditing = true;
-    this.http.get("http://localhost:3000/userList/"+id).subscribe((res:any) => {
-      this.userObject = res;
-    })
-  }
-
-  onUpdate(){
-    this.http.put("http://localhost:3000/userList/"+this.userObject.id,this.userObject).subscribe((res:any) => {
-      this.getData();
+  updateUser() {
+    if (this.isEditing && this.editIndex !== null) {
+      this.users[this.editIndex] = { ...this.userObject };
+      this.updateLocalStorage();
       this.isEditing = false;
+      this.editIndex = null;
       this.clearForm();
-    })
+      this.changeView();
+    }
   }
 
-  onDelete(id:number) {
-    this.http.delete("http://localhost:3000/userList/"+id).subscribe((res:any) => {
-      this.getData();
-    });
-  }
+  users = [
+    { id:"1",firstName: 'John', lastName: 'Doe', phoneNumber: '1234567890', state: 'California', district: 'LA', country: 'USA', email: 'john@example.com', pincode: '90001', gender: 'Male' },
+    { id:"2",firstName: 'Jane', lastName: 'Smith', phoneNumber: '9876543210', state: 'Texas', district: 'Houston', country: 'USA', email: 'jane@example.com', pincode: '77001', gender: 'Female' },
+    { id:"3",firstName: 'Raj', lastName: 'Kumar', phoneNumber: '8765432109', state: 'Maharashtra', district: 'Mumbai', country: 'India', email: 'raj@example.com', pincode: '400001', gender: 'Male' }
+  ];
 
   clearForm(){
     this.userObject = new User();
+    this.isEditing = false;
+    this.editIndex = null;
   }
+
+  deleteUser(userId: string) {
+    this.users = this.users.filter(user => user.id !== userId);
+    this.updateLocalStorage();
+  } 
+
+  editUser(user: any) {
+    const userIndex = this.users.findIndex(u => u.id === user.id);
+    if (userIndex !== -1) {
+      this.userObject = { ...this.users[userIndex] }; // Clone user data into form
+      this.isEditing = true;
+      this.editIndex = userIndex;
+      this.changeView();
+    }
+  }
+
+  updateLocalStorage() {
+    localStorage.setItem('users', JSON.stringify(this.users));
+  }
+
 }
 
-class User {
-  id:any;
-  firstName: string;
-  lastName: string;
-  userName: string;
-  city: string;
-  state: string;
-  zipcode: number;
-  isAgree: boolean;
 
+class User {
+
+  id:any;
+  firstName:string;
+  lastName:string;
+  phoneNumber:any;
+  state:string;
+  district:string;
+  country:string;
+  pincode:any;
+  gender:string;
+  email:string;
+  
   constructor() {
-      (this.id),
-      (this.firstName = ''),
-      (this.lastName = ''),
-      (this.userName = ''),
-      (this.city = ''),
-      (this.state = ''),
-      (this.zipcode = 0),
-      (this.isAgree = false);
+    this.id,
+    this.firstName="",
+    this.lastName="",
+    this.phoneNumber,
+    this.state="",
+    this.district="",
+    this.country="",
+    this.pincode,
+    this.gender="",
+    this.email=""
   }
 }
